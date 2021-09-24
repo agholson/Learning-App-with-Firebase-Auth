bdacac47049f03aef1118637f55e823a20b61ce3
agholson <87455899+agholson@users.noreply.github.com>
2021-09-23 21:11:44 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Shepherd on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
