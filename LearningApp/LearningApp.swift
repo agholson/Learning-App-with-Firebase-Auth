db3459fa2abe3d0f2f5545dd588dd13fd8c35242
Andrew Gholson <test@me.com>
2021-12-26 17:17:33 -0500

    - Added dependencies for Firestore

--- LearningApp/LearningApp.swift
+++ LearningApp/LearningApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct LearningApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             // Initialize our HomeView with the content model environment object
